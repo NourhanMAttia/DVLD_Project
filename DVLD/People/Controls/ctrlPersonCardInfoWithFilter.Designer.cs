90764ff0b89417400efed89822448e1d644b141b
Nourhan <mustafanour751@gmail.com>
2025-03-27 23:28:34 +0300

    Start Working on User Control With Filter

--- DVLD/People/Controls/ctrlPersonCardInfoWithFilter.Designer.cs
+++ DVLD/People/Controls/ctrlPersonCardInfoWithFilter.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.ctrlPersonCardInfo1 = new DVLD.People.Controls.ctrlPersonCardInfo();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddNewPerson = new System.Windows.Forms.Button();
+            this.btnFindPerson = new System.Windows.Forms.Button();
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.cbFilterBy = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,10 +47,10 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btnAddNewPerson);
+            this.groupBox1.Controls.Add(this.btnFindPerson);
+            this.groupBox1.Controls.Add(this.txtFilterValue);
+            this.groupBox1.Controls.Add(this.cbFilterBy);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(19, 14);
@@ -59,6 +59,44 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filter";
+            // 
+            // btnAddNewPerson
+            // 
+            this.btnAddNewPerson.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddNewPerson.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddNewPerson.Image = global::DVLD.Properties.Resources.addPerson24;
+            this.btnAddNewPerson.Location = new System.Drawing.Point(534, 26);
+            this.btnAddNewPerson.Name = "btnAddNewPerson";
+            this.btnAddNewPerson.Size = new System.Drawing.Size(50, 25);
+            this.btnAddNewPerson.TabIndex = 4;
+            this.btnAddNewPerson.UseVisualStyleBackColor = true;
+            // 
+            // btnFindPerson
+            // 
+            this.btnFindPerson.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFindPerson.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFindPerson.Image = global::DVLD.Properties.Resources.search24;
+            this.btnFindPerson.Location = new System.Drawing.Point(469, 26);
+            this.btnFindPerson.Name = "btnFindPerson";
+            this.btnFindPerson.Size = new System.Drawing.Size(50, 25);
+            this.btnFindPerson.TabIndex = 3;
+            this.btnFindPerson.UseVisualStyleBackColor = true;
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.Location = new System.Drawing.Point(290, 26);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(164, 25);
+            this.txtFilterValue.TabIndex = 2;
+            // 
+            // cbFilterBy
+            // 
+            this.cbFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilterBy.FormattingEnabled = true;
+            this.cbFilterBy.Location = new System.Drawing.Point(111, 26);
+            this.cbFilterBy.Name = "cbFilterBy";
+            this.cbFilterBy.Size = new System.Drawing.Size(164, 25);
+            this.cbFilterBy.TabIndex = 1;
             // 
             // label1
             // 
@@ -69,44 +107,6 @@
             this.label1.Size = new System.Drawing.Size(75, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Filter By: ";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(111, 26);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(164, 25);
-            this.comboBox1.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(290, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 25);
-            this.textBox1.TabIndex = 2;
-            // 
-            // button2
-            // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Image = global::DVLD.Properties.Resources.addPerson24;
-            this.button2.Location = new System.Drawing.Point(534, 26);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 25);
-            this.button2.TabIndex = 4;
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Image = global::DVLD.Properties.Resources.search24;
-            this.button1.Location = new System.Drawing.Point(469, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 25);
-            this.button1.TabIndex = 3;
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // ctrlPersonCardInfoWithFilter
             // 
@@ -126,10 +126,10 @@
 
         private ctrlPersonCardInfo ctrlPersonCardInfo1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.ComboBox cbFilterBy;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnFindPerson;
+        private System.Windows.Forms.Button btnAddNewPerson;
     }
 }
